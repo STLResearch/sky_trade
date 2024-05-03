2492d5292ba4aac695f28a12e541670691cf4946
engelsimmanuel <engelmmanuel@gmail.com>
2024-05-03 16:45:36 +0100

    feat: Add more files to project struture, add dependencies for app localization, code generation and testing and setup hydrated storage, service locator and initial theme system

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
+import 'package:sky_ways/core/resources/colors.dart' show seedColor;
+
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) => MaterialApp(
+        theme: ThemeData.from(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: seedColor,
+            brightness: Brightness.dark,
+          ),
+        ),
         onGenerateRoute: _routes,
       );
 
