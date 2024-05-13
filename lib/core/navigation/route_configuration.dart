9f1292662028c16be3abbc0913af9f15d5e849ad
engelsimmanuel <engelmmanuel@gmail.com>
2024-05-13 11:14:23 +0100

    refactor: Rename parent directory of layers from 'features' to 'feature'

--- lib/core/navigation/route_configuration.dart
+++ lib/core/navigation/route_configuration.dart
@@ -2,7 +2,7 @@
 
 import 'package:flutter/material.dart'
     show MaterialPageRoute, Route, RouteSettings;
-import 'package:sky_ways/features/presentation/views/home_screen.dart';
+import 'package:sky_ways/feature/presentation/views/home_screen.dart';
 
 Route routes(RouteSettings settings) => MaterialPageRoute(
       builder: (context) => switch (settings.name) {
