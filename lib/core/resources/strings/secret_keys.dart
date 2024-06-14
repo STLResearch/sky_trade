8d3ae79cec4f494b3e19f08854576057a210687f
engelsimmanuel <engelmmanuel@gmail.com>
2024-06-14 11:02:32 +0100

    feat: Add mapbox map to map screen

--- lib/core/resources/strings/secret_keys.dart
+++ lib/core/resources/strings/secret_keys.dart
@@ -1,5 +1,9 @@
-// Google API
+// Google maps
 const googleMapsApiKey = 'GOOGLE_MAPS_API_KEY';
+
+// Mapbox maps
+const mapboxMapsPublicKey = 'MAPBOX_MAPS_PUBLIC_KEY';
+const mapboxMapsStyleUri = 'MAPBOX_MAPS_STYLE_URI';
 
 // Web3Auth
 const web3AuthClientId = 'WEB_3_AUTH_CLIENT_ID';
---
+++ lib/features/drone_restrictions/presentation/views/map_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' show dotenv;
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+import 'package:sky_ways/core/resources/strings/secret_keys.dart'
+    show mapboxMapsPublicKey, mapboxMapsStyleUri;
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  late final MapboxMap? _mapboxMap;
+
+  @override
+  void initState() {
+    MapboxOptions.setAccessToken(
+      dotenv.env[mapboxMapsPublicKey]!,
+    );
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _mapboxMap?.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: MapWidget(
+          styleUri: dotenv.env[mapboxMapsStyleUri]!,
+          onMapCreated: (mapboxMap) => _mapboxMap = mapboxMap,
+        ),
+      );
+}
