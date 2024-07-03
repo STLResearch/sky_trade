ff6de34eb1a0a9c762cb761e4821f45b5e3ad758
engelsimmanuel <engelmmanuel@gmail.com>
2024-07-03 10:22:52 +0100

    feat: Add new asset path file contains string that reference new SVG assets

---
+++ lib/core/resources/strings/asset_paths.dart
@@ -0,0 +1,7 @@
+const assetImagesPath = 'assets/images/';
+
+const accountAssetPath = '$assetImagesPath/account.svg';
+const communityAssetPath = '$assetImagesPath/community.svg';
+const flightAssetPath = '$assetImagesPath/flight.svg';
+const mapAssetPath = '$assetImagesPath/map.svg';
+const weatherAssetPath = '$assetImagesPath/weather.svg';
