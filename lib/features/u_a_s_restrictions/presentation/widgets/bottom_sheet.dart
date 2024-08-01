e9bb70a6590409c7b823089fbffb4bdddbe9949d
engelsimmanuel <engelmmanuel@gmail.com>
2024-08-01 21:07:53 +0100

    fix: Clean up bottom_sheet.dart

--- lib/features/u_a_s_restrictions/presentation/widgets/bottom_sheet.dart
+++ lib/features/u_a_s_restrictions/presentation/widgets/bottom_sheet.dart
@@ -1,31 +1,54 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'
+    show
+        BorderRadiusDirectional,
+        BoxDecoration,
+        BuildContext,
+        Center,
+        Column,
+        Container,
+        CrossAxisAlignment,
+        Divider,
+        EdgeInsetsDirectional,
+        ElevatedButton,
+        Expanded,
+        FontWeight,
+        MainAxisAlignment,
+        MainAxisSize,
+        Padding,
+        Radius,
+        Row,
+        Scaffold,
+        SizedBox,
+        StatelessWidget,
+        Text,
+        Theme,
+        Widget,
+        showModalBottomSheet;
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_svg/svg.dart' show SvgPicture;
 import 'package:sky_ways/core/resources/colors.dart'
-    show hex1E1E1E, hex9D9D9D, hexD9D9D9, hexEBEBEB, hexF4F4F4, hexFFFFFF;
+    show hex1E1E1E, hex9D9D9D, hexF4F4F4;
 import 'package:sky_ways/core/resources/numbers/ui.dart'
     show
         eightDotNil,
-        eighteenDotNil,
         fifteenDotNil,
-        fortyThreeDotNil,
         fourteenDotNil,
         nineDotNil,
         oneDotNil,
         seventyDotNil,
         tenDotNil,
+        thirteenDotNil,
         thirtyDotNil,
         thirtyTwoDotNil,
-        twelveDotNil,
         twentyDotNil,
         twentyFiveDotEightFive,
         twentyFourDotNil,
         twentyNineDotNil,
+        twentyOneDotNil,
         twentySixDotNil,
-        twentyTwoDotNil,
-        twoHundredFortyNineDotSevenNine;
+        twentyTwoDotNil;
 import 'package:sky_ways/core/resources/strings/asset_paths.dart'
-    show arrowAssetPath, exitAssetPath, locationAssetPath;
+    show arrowAssetPath, infoAssetPath, locationProhibitedAssetPath;
 
 class BottomSheet extends StatelessWidget {
   const BottomSheet({super.key});
@@ -39,211 +62,185 @@ class BottomSheet extends StatelessWidget {
 class BottomSheetState extends StatelessWidget {
   const BottomSheetState({super.key});
 
-  void _showBottomSheet(BuildContext context) {
-    showModalBottomSheet(
-      context: context,
-      builder: (BuildContext context) {
-        return Container(
+  Future<void> _showBottomSheet(BuildContext context) =>
+      showModalBottomSheet<void>(
+        context: context,
+        builder: (BuildContext context) => Container(
           decoration: const BoxDecoration(
-            color: hexFFFFFF,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(thirtyDotNil),
-              topRight: Radius.circular(thirtyDotNil),
+            borderRadius: BorderRadiusDirectional.vertical(
+              top: Radius.circular(
+                thirtyDotNil,
+              ),
             ),
           ),
-          child: Padding(
-            padding: const EdgeInsets.only(
-              left: twentyNineDotNil,
-              top: fortyThreeDotNil,
-              right: twentyNineDotNil,
-              bottom: twentyDotNil,
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Container(
-                  width: seventyDotNil,
-                  height: nineDotNil,
-                  decoration: BoxDecoration(
-                    color: hexD9D9D9,
-                    borderRadius: BorderRadius.circular(tenDotNil),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const SizedBox(
+                height: thirteenDotNil,
+              ),
+              Container(
+                width: seventyDotNil,
+                height: nineDotNil,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).bottomSheetTheme.dragHandleColor,
+                  borderRadius: BorderRadiusDirectional.circular(
+                    tenDotNil,
                   ),
                 ),
-                const SizedBox(height: fifteenDotNil),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+              ),
+              Padding(
+                padding: const EdgeInsetsDirectional.only(
+                  start: twentyNineDotNil,
+                  end: twentyNineDotNil,
+                  top: twentyOneDotNil,
+                  bottom: twentyDotNil,
+                ),
+                child: Column(
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    SvgPicture.asset(
-                      exitAssetPath,
-                      width: twentyFourDotNil,
-                      height: twentyFourDotNil,
-                    ),
-                    const SizedBox(width: tenDotNil),
-                    Expanded(
-                      child: Text(
-                        AppLocalizations.of(context)!.flightRestrictedAirspace,
-                        textAlign: TextAlign.left,
-                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                              fontWeight: FontWeight.w700,
-                              fontSize: twentyTwoDotNil,
-                              height: thirtyTwoDotNil / twentyTwoDotNil,
-                              color: hex1E1E1E,
-                            ),
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: fifteenDotNil),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        height: oneDotNil,
-                        decoration: const BoxDecoration(
-                          color: hexEBEBEB,
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        SvgPicture.asset(
+                          infoAssetPath,
+                          width: twentyFourDotNil,
+                          height: twentyFourDotNil,
                         ),
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: fifteenDotNil),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Row(
-                        children: [
-                          SvgPicture.asset(
-                            locationAssetPath,
-                            width: thirtyDotNil,
-                            height: thirtyDotNil,
+                        const SizedBox(
+                          width: tenDotNil,
+                        ),
+                        Expanded(
+                          child: Text(
+                            AppLocalizations.of(context)!
+                                .flightRestrictedAirspace,
+                            style:
+                                Theme.of(context).textTheme.bodyLarge?.copyWith(
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: twentyTwoDotNil,
+                                      height: thirtyTwoDotNil / twentyTwoDotNil,
+                                      color: hex1E1E1E,
+                                    ),
                           ),
-                          const SizedBox(width: fourteenDotNil),
-                          Column(
-                            mainAxisSize: MainAxisSize.min,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: fifteenDotNil,
+                    ),
+                    const Divider(
+                      height: oneDotNil,
+                    ),
+                    const SizedBox(
+                      height: fifteenDotNil,
+                    ),
+                    Row(
+                      children: [
+                        SvgPicture.asset(
+                          locationProhibitedAssetPath,
+                          width: thirtyDotNil,
+                          height: thirtyDotNil,
+                        ),
+                        const SizedBox(
+                          width: fourteenDotNil,
+                        ),
+                        Expanded(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
                             crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.min,
                             children: [
                               Text(
                                 AppLocalizations.of(context)!.address,
-                                textAlign: TextAlign.left,
                                 style: Theme.of(context)
                                     .textTheme
-                                    .bodyMedium
+                                    .bodySmall
                                     ?.copyWith(
-                                      fontWeight: FontWeight.w400,
-                                      fontSize: twelveDotNil,
-                                      height: eighteenDotNil / twelveDotNil,
                                       color: hex9D9D9D,
                                     ),
                               ),
-                              SizedBox(
-                                width: twoHundredFortyNineDotSevenNine,
-                                child: Text(
-                                  AppLocalizations.of(context)!.specificAddress,
-                                  textAlign: TextAlign.left,
+                              Text(
+                                AppLocalizations.of(context)!.specificAddress,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodySmall
+                                    ?.copyWith(
+                                      fontWeight: FontWeight.w500,
+                                      color: hex1E1E1E,
+                                    ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: fifteenDotNil,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: hexF4F4F4,
+                        borderRadius: BorderRadiusDirectional.circular(
+                          eightDotNil,
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsetsDirectional.all(
+                          tenDotNil,
+                        ),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              AppLocalizations.of(context)!
+                                  .bottomSheetParagraph,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodySmall
+                                  ?.copyWith(
+                                    color: hex1E1E1E,
+                                  ),
+                            ),
+                            const SizedBox(
+                              height: tenDotNil,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  AppLocalizations.of(context)!
+                                      .federalAviationAdministration,
                                   style: Theme.of(context)
                                       .textTheme
-                                      .bodyLarge
+                                      .bodySmall
                                       ?.copyWith(
                                         fontWeight: FontWeight.w500,
-                                        fontSize: twelveDotNil,
-                                        height: eighteenDotNil / twelveDotNil,
                                         color: hex1E1E1E,
                                       ),
                                 ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: fifteenDotNil),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: hexF4F4F4,
-                          borderRadius: BorderRadius.circular(eightDotNil),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(tenDotNil),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                children: [
-                                  Expanded(
-                                    child: Text(
-                                      AppLocalizations.of(context)!
-                                          .bottomSheetParagraph,
-                                      textAlign: TextAlign.left,
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .bodyMedium
-                                          ?.copyWith(
-                                            fontWeight: FontWeight.w400,
-                                            fontSize: twelveDotNil,
-                                            height:
-                                                eighteenDotNil / twelveDotNil,
-                                            color: hex1E1E1E,
-                                          ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              const SizedBox(height: tenDotNil),
-                              Row(
-                                children: [
-                                  Expanded(
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Text(
-                                          AppLocalizations.of(context)!
-                                              .federalAviationAdministration,
-                                          textAlign: TextAlign.left,
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .bodyLarge
-                                              ?.copyWith(
-                                                fontWeight: FontWeight.w500,
-                                                fontSize: twelveDotNil,
-                                                height: eighteenDotNil /
-                                                    twelveDotNil,
-                                                color: hex1E1E1E,
-                                              ),
-                                        ),
-                                        const SizedBox(width: tenDotNil),
-                                        SvgPicture.asset(
-                                          arrowAssetPath,
-                                          width: twentyFiveDotEightFive,
-                                          height: twentySixDotNil,
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
+                                const SizedBox(
+                                  width: tenDotNil,
+                                ),
+                                SvgPicture.asset(
+                                  arrowAssetPath,
+                                  width: twentyFiveDotEightFive,
+                                  height: twentySixDotNil,
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
                       ),
                     ),
+                    // const SizedBox(height: fifteenDotNil),
                   ],
                 ),
-                const SizedBox(height: fifteenDotNil),
-              ],
-            ),
+              ),
+            ],
           ),
-        );
-      },
-    );
-  }
+        ),
+      );
 
   @override
   Widget build(BuildContext context) {
