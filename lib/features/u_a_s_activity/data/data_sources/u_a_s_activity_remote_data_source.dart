5a926e78ac283df5ca82914e22531a10e00f904c
Darrel <darrelasare3@gmail.com>
2024-08-23 15:01:44 -0400

    refactor: Modify u_a_s_activity_remote_data_source.dart to use websocket client

--- lib/features/u_a_s_activity/data/data_sources/u_a_s_activity_remote_data_source.dart
+++ lib/features/u_a_s_activity/data/data_sources/u_a_s_activity_remote_data_source.dart
@@ -1,16 +1,39 @@
-import 'package:dio/dio.dart' show Response;
+import 'package:sky_ways/core/errors/exceptions/u_a_s_activity_exception.dart';
 import 'package:sky_ways/core/utils/clients/network_client.dart'
-    show NetworkClient;
+    show WebSocketClient;
 import 'package:sky_ways/core/utils/clients/response_handler.dart';
+import 'package:sky_ways/features/u_a_s_activity/data/models/u_a_s_model.dart'
+    show UASModel;
 
-abstract interface class UASActivityRemoteDataSource {}
+abstract interface class UASActivityRemoteDataSource {
+  Stream<List<UASModel>> getUASActivitiesAround({
+    required String geoHash,
+  });
+}
 
 final class UASActivityRemoteDataSourceImplementation
     with ResponseHandler
     implements UASActivityRemoteDataSource {
   const UASActivityRemoteDataSourceImplementation(
-    NetworkClient<Response> networkClient,
-  ) : _networkClient = networkClient;
+    WebSocketClient webSocketClient,
+  ) : _webSocketClient = webSocketClient;
 
-  final NetworkClient<Response> _networkClient;
+  final WebSocketClient _webSocketClient;
+
+  @override
+  Stream<List<UASModel>> getUASActivitiesAround({
+    required String geoHash,
+  }) =>
+      transformResponse<UASActivityException, List<dynamic>, List<UASModel>>(
+        communicationInitiator: () =>
+            _webSocketClient.messages as Stream<List<dynamic>>,
+        onData: (jsonList) => jsonList
+            .map(
+              (json) => UASModel.fromJson(
+                json as Map<String, dynamic>,
+              ),
+            )
+            .toList(),
+        onError: UASActivityException.new,
+      );
 }
