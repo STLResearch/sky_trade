3f1dc0522c51f43e851a030301f0366e65ab97ae
Brez18 <93505170+Brez18@users.noreply.github.com>
2024-09-11 16:12:45 +0530

    refactor: Clean up code in data and domain layer of remote_id_receiver feature

--- lib/features/remote_id_receiver/data/repositories/remote_id_receiver_repository_implementation.dart
+++ lib/features/remote_id_receiver/data/repositories/remote_id_receiver_repository_implementation.dart
@@ -1,59 +1,25 @@
 import 'dart:async';
 
-import 'package:sky_ways/core/utils/enums/local.dart' as enums show PermissionStatus;
 import 'package:flutter_opendroneid/flutter_opendroneid.dart';
 import 'package:flutter_opendroneid/models/message_container.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:sky_ways/features/remote_id_receiver/domain/entities/remote_id_message.dart';
 import 'package:sky_ways/features/remote_id_receiver/domain/repositories/remote_id_receiver_repository.dart';
 
 final class BluetoothReceiver implements RemoteIdReceiverRepository {
   @override
-  UsedTechnologies usedTechnology = UsedTechnologies.Bluetooth;
-  @override
-  late StreamController<RemoteIDMessage> streamController;
-  @override
-  late StreamSubscription<MessageContainer> streamSubscription;
+  Stream<RemoteIDMessage> getRemoteIDMessageStream(
+    UsedTechnologies technology,
+  ) {
+    late final StreamController<RemoteIDMessage> streamController;
+    late final StreamSubscription<MessageContainer> streamSubscription;
 
-  @override
-  Future<enums.PermissionStatus> requestRequiredPermissions() async {
-    final statuses = await [
-      Permission.bluetoothScan,
-      Permission.bluetoothConnect,
-    ].request();
-
-    final permissionNotGranted =
-        statuses.containsValue(PermissionStatus.denied) ||
-            statuses.containsValue(PermissionStatus.permanentlyDenied) ||
-            statuses.containsValue(PermissionStatus.restricted);
-
-    if (permissionNotGranted) {
-      return enums.PermissionStatus.notGranted;
-    }
-
-    return enums.PermissionStatus.granted;
-  }
-
-  @override
-  Future<bool> checkTechnologiesEnabled() async {
-    return await Permission.bluetooth.serviceStatus.isEnabled &&
-        await Permission.location.serviceStatus.isEnabled;
-  }
-
-  @override
-  Stream<RemoteIDMessage> getRemoteIDMessageStream() {
     streamController = StreamController<RemoteIDMessage>(
       onListen: () async {
-        if (await requestRequiredPermissions() == enums.PermissionStatus.granted
-            && await checkTechnologiesEnabled()) {
-          await FlutterOpenDroneId.startScan(UsedTechnologies.Bluetooth);
-          streamSubscription =
-              FlutterOpenDroneId.allMessages.listen((MessageContainer message) {
-                streamController.add(RemoteIDMessage(message));
-              });
-        } else {
-          //return left error
-        }
+        await FlutterOpenDroneId.startScan(technology);
+        streamSubscription =
+            FlutterOpenDroneId.allMessages.listen((MessageContainer message) {
+          streamController.add(RemoteIDMessage(message));
+        });
       },
       onCancel: () {
         FlutterOpenDroneId.stopScan();
--- lib/features/remote_id_receiver/domain/entities/remote_id_message.dart
+++ lib/features/remote_id_receiver/domain/entities/remote_id_message.dart
@@ -1,21 +1,21 @@
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_opendroneid/models/message_container.dart';
 
 @immutable
 class RemoteIDMessage extends MessageContainer {
-
-  factory RemoteIDMessage(MessageContainer message){
+  factory RemoteIDMessage(MessageContainer message) {
     return RemoteIDMessage._internal(
-        macAddress: message.macAddress,
-        lastUpdate: message.lastUpdate,
-        source: message.source,
-        lastMessageRssi: message.lastMessageRssi,
-        basicIdMessages: message.basicIdMessages,
-        locationMessage: message.locationMessage,
-        operatorIdMessage: message.operatorIdMessage,
-        selfIdMessage: message.selfIdMessage,
-        authenticationMessage: message.authenticationMessage,
-        systemDataMessage: message.systemDataMessage,
+      macAddress: message.macAddress,
+      lastUpdate: message.lastUpdate,
+      source: message.source,
+      lastMessageRssi: message.lastMessageRssi,
+      basicIdMessages: message.basicIdMessages,
+      locationMessage: message.locationMessage,
+      operatorIdMessage: message.operatorIdMessage,
+      selfIdMessage: message.selfIdMessage,
+      authenticationMessage: message.authenticationMessage,
+      systemDataMessage: message.systemDataMessage,
     );
   }
 
