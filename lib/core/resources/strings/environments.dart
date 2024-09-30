6e34e8fdf45d7f0c2bdc13fbda639a36ea29e83a
engelsimmanuel <engelmmanuel@gmail.com>
2024-09-30 12:14:55 +0100

    feat: Partly implement In-App update feature for Android

--- lib/core/resources/strings/environments.dart
+++ lib/core/resources/strings/environments.dart
@@ -8,3 +8,12 @@ const flavours = 'FLUTTER_APP_FLAVOR';
 const devEnvironment = 'dev';
 const stageEnvironment = 'stage';
 const liveEnvironment = 'live';
+
+// Platform specific code
+const inAppUpdateChannelName = 'IN_APP_UPDATE_CHANNEL';
+const checkAndStartUpdateMethodCall = 'CHECK_AND_START_UPDATE';
+const registerUpdateStateListenerAndCheckAndResumeStalledImmediateUpdateIfAnyMethodCall =
+    'REGISTER_UPDATE_STATE_LISTENER_AND_CHECK_AND_RESUME_STALLED_IMMEDIATE_UPDATE_IF_ANY';
+const installFlexibleUpdateMethodCall = 'INSTALL_FLEXIBLE_UPDATE';
+const unregisterUpdateStateListenerMethodCall =
+    'UNREGISTER_UPDATE_STATE_LISTENER';
