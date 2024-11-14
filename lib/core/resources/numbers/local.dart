c0a9e931398539b8200c0fce1142f81f29fe14f5
engelsimmanuel <engelmmanuel@gmail.com>
2024-11-14 18:33:11 +0100

    refactor: Clean up search autocomplete token rotation implementation

--- lib/core/resources/numbers/local.dart
+++ lib/core/resources/numbers/local.dart
@@ -1,3 +1,7 @@
+const zero = 0;
+const one = 1;
 const eleven = 11;
 const sixteen = 16;
 const thirtyTwo = 32;
+const fifty = 50;
+const sixty = 60;
--- lib/features/search_autocomplete/data/data_sources/search_autocomplete_local_data_source.dart
+++ lib/features/search_autocomplete/data/data_sources/search_autocomplete_local_data_source.dart
@@ -1,12 +1,9 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:sky_trade/core/resources/numbers/local.dart'
+    show fifty, one, sixty, zero;
 import 'package:sky_trade/core/resources/strings/local.dart'
-    show sessionTokenKey;
+    show sessionTokenAgeKey, sessionTokenKey, sessionTokenUsageCountKey;
 import 'package:uuid_v4/uuid_v4.dart';
-
-const sessionTokenCountKey = 'session_token_count';
-const sessionTokenTimestampKey = 'session_token_timestamp';
-const int maxCallCount = 50;
-const int maxTokenAgeMinutes = 60;
 
 abstract interface class SearchAutocompleteLocalDataSource {
   Future<String> get sessionToken;
@@ -22,56 +19,154 @@ final class SearchAutocompleteLocalDataSourceImplementation
 
   @override
   Future<String> get sessionToken async {
-    final cachedSessionToken = await _getCachedSessionToken();
-    final callCount = await _getCallCount();
-    final tokenTimestamp = await _getTokenTimestamp();
+    var sessionToken = await _getCachedSessionToken();
 
-    final isTokenExpired = tokenTimestamp != null &&
-        DateTime.now().difference(tokenTimestamp).inMinutes >=
-            maxTokenAgeMinutes;
+    final shouldRotateSessionToken = await _sessionTokenNeedsRotation();
 
-    if (cachedSessionToken != null &&
-        callCount < maxCallCount &&
-        !isTokenExpired) {
-      await _incrementCallCount(callCount);
-      return cachedSessionToken;
+    if (shouldRotateSessionToken) {
+      sessionToken = _generateNewSessionToken();
+
+      await _cacheSessionToken(
+        sessionToken,
+      );
+
+      await _cacheSessionTokenUsageCount(
+        zero,
+      );
+
+      await _cacheSessionTokenAge(
+        DateTime.now(),
+      );
     }
 
+    await _incrementSessionTokenUsageCountBy(
+      one,
+    );
+
+    return sessionToken;
+  }
+
+  Future<String> _getCachedSessionToken() async {
+    var sessionToken = await _flutterSecureStorage.read(
+      key: sessionTokenKey,
+    );
+
+    if (sessionToken == null) {
+      sessionToken = _generateNewSessionToken();
+
+      await _cacheSessionToken(
+        sessionToken,
+      );
+    }
+
+    return sessionToken;
+  }
+
+  Future<bool> _sessionTokenNeedsRotation() async {
+    final sessionTokenUsageCount = await _getSessionTokenUsageCount();
+
+    final sessionTokenAgeInMinutes = await _getSessionTokenAgeInMinutes();
+
+    return sessionTokenUsageCount == fifty || sessionTokenAgeInMinutes >= sixty;
+  }
+
+  String _generateNewSessionToken() {
     final uuidV4 = UUIDv4();
-    final newSessionToken = uuidV4.toString();
-    await _cacheSessionToken(newSessionToken);
+    final sessionToken = uuidV4.toString();
 
-    return newSessionToken;
+    return sessionToken;
   }
 
-  Future<void> _cacheSessionToken(String token) async {
-    await _flutterSecureStorage.write(key: sessionTokenKey, value: token);
-    await _flutterSecureStorage.write(key: sessionTokenCountKey, value: '0');
-    await _flutterSecureStorage.write(
-      key: sessionTokenTimestampKey,
-      value: DateTime.now().toIso8601String(),
+  Future<void> _cacheSessionToken(
+    String token,
+  ) =>
+      _flutterSecureStorage.write(
+        key: sessionTokenKey,
+        value: token,
+      );
+
+  Future<void> _incrementSessionTokenUsageCountBy(
+    int increment,
+  ) async {
+    final sessionTokenUsageCount = await _getSessionTokenUsageCount();
+
+    final incrementedSessionTokenUsageCount =
+        sessionTokenUsageCount + increment;
+
+    await _cacheSessionTokenUsageCount(
+      incrementedSessionTokenUsageCount,
     );
   }
 
-  Future<String?> _getCachedSessionToken() =>
-      _flutterSecureStorage.read(key: sessionTokenKey);
-
-  Future<int> _getCallCount() async {
-    final countStr =
-        await _flutterSecureStorage.read(key: sessionTokenCountKey);
-    return int.tryParse(countStr ?? '0') ?? 0;
-  }
-
-  Future<DateTime?> _getTokenTimestamp() async {
-    final timestampStr =
-        await _flutterSecureStorage.read(key: sessionTokenTimestampKey);
-    return timestampStr != null ? DateTime.tryParse(timestampStr) : null;
-  }
-
-  Future<void> _incrementCallCount(int currentCount) async {
-    await _flutterSecureStorage.write(
-      key: sessionTokenCountKey,
-      value: (currentCount + 1).toString(),
+  Future<int> _getSessionTokenUsageCount() async {
+    var sessionTokenUsageCountString = await _flutterSecureStorage.read(
+      key: sessionTokenUsageCountKey,
     );
+
+    if (sessionTokenUsageCountString == null) {
+      const count = zero;
+
+      sessionTokenUsageCountString = count.toString();
+
+      await _cacheSessionTokenUsageCount(
+        count,
+      );
+    }
+
+    final sessionTokenUsageCount = int.parse(
+      sessionTokenUsageCountString,
+    );
+
+    return sessionTokenUsageCount;
   }
+
+  Future<void> _cacheSessionTokenUsageCount(
+    int count,
+  ) =>
+      _flutterSecureStorage.write(
+        key: sessionTokenUsageCountKey,
+        value: count.toString(),
+      );
+
+  Future<int> _getSessionTokenAgeInMinutes() async {
+    final sessionTokenAge = await _getSessionTokenAge();
+
+    final sessionTokenAgeInMinutes = DateTime.now()
+        .difference(
+          sessionTokenAge,
+        )
+        .inMinutes;
+
+    return sessionTokenAgeInMinutes;
+  }
+
+  Future<DateTime> _getSessionTokenAge() async {
+    var iso8601AgeString = await _flutterSecureStorage.read(
+      key: sessionTokenAgeKey,
+    );
+
+    if (iso8601AgeString == null) {
+      final now = DateTime.now();
+
+      iso8601AgeString = now.toIso8601String();
+
+      await _cacheSessionTokenAge(
+        now,
+      );
+    }
+
+    final age = DateTime.parse(
+      iso8601AgeString,
+    );
+
+    return age;
+  }
+
+  Future<void> _cacheSessionTokenAge(
+    DateTime age,
+  ) =>
+      _flutterSecureStorage.write(
+        key: sessionTokenAgeKey,
+        value: age.toIso8601String(),
+      );
 }
