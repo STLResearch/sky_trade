a4a8fdb57ec92df4d1a6c8390e83d3eb1b102cfb
engelsimmanuel <engelmmanuel@gmail.com>
2024-11-15 14:38:06 +0100

    fix: Remove unimplemented features from map overlay ahead of publishing to Solana dApp Store

--- lib/features/u_a_s_restrictions/presentation/widgets/menu.dart
+++ lib/features/u_a_s_restrictions/presentation/widgets/menu.dart
@@ -19,7 +19,7 @@ import 'package:flutter_bloc/flutter_bloc.dart' show ReadContext;
 import 'package:sky_trade/core/assets/generated/assets.gen.dart' show Assets;
 import 'package:sky_trade/core/resources/colors.dart' show hex1D1E2D;
 import 'package:sky_trade/core/resources/numbers/ui.dart'
-    show five, twentyDotNil;
+    show four, twentyDotNil;
 import 'package:sky_trade/core/resources/strings/routes.dart'
     show helpRoutePath, insightsRoutePath;
 import 'package:sky_trade/core/utils/enums/ui.dart' show MenuItem;
@@ -52,7 +52,7 @@ class Menu extends StatelessWidget {
           ),
         ),
         menuChildren: List<MenuItemButton>.generate(
-          five,
+          four,
           (index) => MenuItemButton(
             leadingIcon: _computeMenuItemLeadingIconUsing(
               context,
@@ -82,7 +82,6 @@ class Menu extends StatelessWidget {
       switch (MenuItem.values[index]) {
         MenuItem.insights => Assets.svgs.insights.svg(),
         MenuItem.about => Assets.svgs.iconDroneAbout.svg(),
-        MenuItem.settings => Assets.svgs.settings.svg(),
         MenuItem.help => Assets.svgs.help.svg(),
         MenuItem.logout => Assets.svgs.logout.svg(),
       };
@@ -100,7 +99,6 @@ class Menu extends StatelessWidget {
         MenuItem.about => () => AboutDialog.show(
               context,
             ),
-        MenuItem.settings => () {},
         MenuItem.help => () => Navigator.of(
               context,
             ).pushNamed(
@@ -140,7 +138,6 @@ class Menu extends StatelessWidget {
       switch (MenuItem.values[index]) {
         MenuItem.insights => context.localize.insights,
         MenuItem.about => context.localize.about,
-        MenuItem.settings => context.localize.settings,
         MenuItem.help => context.localize.help,
         MenuItem.logout => context.localize.logout,
       };
