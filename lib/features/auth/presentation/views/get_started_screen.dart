258498cf9013874a20418ff82a7c1819a0077173
engelsimmanuel <engelmmanuel@gmail.com>
2025-03-26 15:25:38 +0100

    build, chore: Use latest approach for setting up Flavours on Android and iOS

--- lib/features/auth/presentation/views/get_started_screen.dart
+++ lib/features/auth/presentation/views/get_started_screen.dart
@@ -19,6 +19,7 @@ import 'package:flutter/material.dart'
         TextAlign,
         Theme,
         Widget;
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart'
     show
         BlocBuilder,
